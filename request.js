f3394b35dae703eafcd248e9d1b69c49f448615e
Михаил <vmv@ih882012.vds.myihor.ru>
2018-02-01 16:35:32 +0300

    serial commiting

--- request.js
+++ request.js
@@ -5,15 +5,18 @@ $(document).ready(function(){
   // находим необходимый селектор
   	var btn = document.getElementById('request');
 	var resp = document.getElementById('main');
+	var sndr = document.getElementById('#send');
 
-	$.ajax({
-		method: "GET",
-		url: "info.php",
-		data: {name : "Juin", loc : "Santa Barbara"}
-	})
-	.done(function( msg ) {
-    		alert( msg );
-  	});
+	$('#send').click(function(){
 
+		$.ajax({
+			method: "GET",
+			url: "info.php",
+			data: {name : "Juin", loc : "Santa Barbara"}
+		})
+		.done(function( msg ) {
+    			$(btn).html(msg);
+  		});
+	});
 
 });
