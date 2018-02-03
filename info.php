dfbe5f6630c212b7ee56245931364f477dbc68e0
vmv <vmv@nxt.ru>
2018-02-03 11:47:15 +0300

    auto commit

--- info.php
+++ info.php
@@ -1,13 +1,43 @@
 <?php
 	//Подключаемся к базе
-	$mysqli = new mysqli("127.0.0.1", "root", "23272829", "linux");
+	$mysqli = new mysqli("127.0.0.1", "root", "23272829", "work");
 
-	$db_list = mysqli_query($mysqli, 'SHOW TABLES');
+//	print($mysqli->connect_error);
+//	print_r($_GET);
+//	echo $_GET[unit];
 
-	$row = mysqli_fetch_assoc($db_list);
+//	$db_list = mysqli_query($mysqli, 'SHOW TABLES');
 
-	print_r($row);
+//	$row = mysqli_fetch_assoc($db_list);
+
+//	print_r($_GET);
+
+	$query_string = "INSERT INTO parts (unit, point, brokes, fix, note) VALUES (\"$_GET[unit]\", \"$_GET[point]\", \"$_GET[brokes]\", \"$_GET[fix]\", \"$_GET[note]\")";
+	echo $query_string;
+	$db_list = mysqli_query($mysqli, $query_string);
+
+	if($db_list == FALSE){
+		print("Query is false!");
+	}
+//	$row = mysqli_fetch_assoc($db_list);
+//	print_r($row);
 
 	//Закрываем контекст работы с базами
 	mysqli_close($mysqli);
+
+/*
+server = 127.0.0.1;
+db = work;
+table = parts;
+
+id INT AUTO INCREMENT,
+time TIMESTAMP,
+unit TINYTEXT,
+point TINYTEXT,
+brokes TEXT,
+fix TEXT,
+note TEXT,
+*/
+
+
 ?>
--- request.js
+++ request.js
@@ -3,14 +3,16 @@ $(document).ready(function(){
   'use strict';
 
 	$('#request').click(function(){
-
+		var unit = $("input[name='Dbname']").val();
+		var point = $("input[name='Agent']").val();
 		$.ajax({
 			method: "GET",
 			url: "info.php",
-			data: {Dbname: "linux", name : "Juin", loc : "Santa Barbara"}
+			data: {unit: unit, point: point, brokes: "NULL", fix: "NULL", note: "NULL"}
 		})
 		.done(function( msg ) {
-    			$('#request').html(msg);
+//    			$('#request').html(msg);
+//    			$("input[name='Agent']").val(unit);
   		});
 	});
 
